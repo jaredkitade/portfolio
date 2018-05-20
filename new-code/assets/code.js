041ae6d61b32036f7185227366a2b24271541e5a
jaredkitade <jkitade@cca.edu>
2018-05-20 13:43:03 -0700

    hamburger active/overlay close

--- new-code/assets/code.js
+++ new-code/assets/code.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
-	var hamburger = document.querySelector(".hamburger");
+	var hamburger = document.querySelector('.hamburger');
 
-	hamburger.addEventListener("click", function() {
-		hamburger.classList.toggle("is-active");
+	hamburger.addEventListener('click', function() {
+		hamburger.classList.toggle('is-active');
 	});
 
 	$('.hamburger').click(function(){
-		$('.menu').toggleClass("open");
+		$('.menu').toggleClass('open');
+		$('.clickoff').toggleClass('open-clickoff');
+	});
+
+	$('.clickoff').click(function(){
+		hamburger.classList.toggle('is-active');
+		$('.menu').toggleClass('open');
+		$('.clickoff').toggleClass('open-clickoff');
 	});
 });	
