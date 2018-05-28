35aaa02d3e7ea804e8f9721017b926352b0543f5
jaredkitade <jkitade@cca.edu>
2018-05-28 01:43:08 -0700

    update navigation bar, slide animation

--- new-code/assets/code.js
+++ new-code/assets/code.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 		$('body').toggleClass('scroll');
 		$('.all').toggleClass('body-menu');
 		$('.project-right').toggleClass('body-menu');
+		$('.menu-title').toggleClass('menu-title-active');
 	});
 
 	$('.clickoff').click(function(){
@@ -19,5 +20,46 @@ $(document).ready(function(){
 		$('.clickoff').toggleClass('open-clickoff');
 		$('body').toggleClass('scroll');
 		$('.all').toggleClass('body-menu');
+		$('.menu-title').toggleClass('menu-title-active');
 	});
+
+	var logo = [
+	  'assets/logos/j-domaine-1.svg',
+	  'assets/logos/j-domaine-2.svg',
+	  'assets/logos/j-domaine-3.svg',
+	  'assets/logos/j-domaine-4.svg',
+	  'assets/logos/j-domaine-5.svg',
+	  'assets/logos/j-domaine-6.svg',
+	  'assets/logos/j-domaine-7.svg',
+	  'assets/logos/j-domaine-8.svg',
+	  'assets/logos/j-din-1.svg',
+	  'assets/logos/j-din-2.svg',
+	  'assets/logos/j-din-3.svg',
+	  'assets/logos/j-din-4.svg',
+	  'assets/logos/j-din-5.svg',
+	  'assets/logos/j-din-6.svg',
+	  'assets/logos/j-din-7.svg',
+	  'assets/logos/j-din-8.svg',
+	  'assets/logos/j-bodoni-1.svg',
+	  'assets/logos/j-bodoni-2.svg',
+	  'assets/logos/j-bodoni-3.svg',
+	  'assets/logos/j-bodoni-4.svg',
+	  'assets/logos/j-bodoni-5.svg',
+	  'assets/logos/j-bodoni-6.svg',
+	  'assets/logos/j-bodoni-7.svg',
+	  'assets/logos/j-bodoni-8.svg',
+	  'assets/logos/j-planeta-1.svg',
+	  'assets/logos/j-planeta-2.svg',
+	  'assets/logos/j-planeta-3.svg',
+	  'assets/logos/j-planeta-4.svg',
+	  'assets/logos/j-planeta-5.svg',
+	  'assets/logos/j-planeta-6.svg',
+	  'assets/logos/j-planeta-7.svg',
+	  'assets/logos/j-planeta-8.svg'
+	];
+
+	var size = logo.length
+	var x = Math.floor(size*Math.random())
+	document.getElementById('logo-image').src=logo[x];
+
 });	
