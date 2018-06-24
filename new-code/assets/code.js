720e73ca2de5e26477c90883bec66cfca49d2f5a
jaredkitade <jkitade@cca.edu>
2018-06-23 20:15:17 -0700

    hamburger hover animations

--- new-code/assets/code.js
+++ new-code/assets/code.js
@@ -8,6 +8,100 @@ $(document).ready(function(){
 		hamburger.classList.toggle('is-active');
 	});
 
+	// $(document).ready(function(){
+	// 	$('.hamburger').click(function(){
+	// 		$('.menu').toggleClass('open');
+	// 		$('.menu-bottom').toggleClass('open');
+	// 		$('.clickoff').toggleClass('open-clickoff');
+	// 		$('body').toggleClass('scroll');
+	// 		$('.thumbnail-portrait img').toggleClass('menu-short');
+	// 		$('.thumbnail-landscape img').toggleClass('menu-short');
+	// 		$('.thumbnail-button').toggleClass('menu-wide');
+	// 		$('.project-right').toggleClass('menu-wide');
+	// 		$('.project-left').toggleClass('menu-short');
+	// 		$('.project-hero-image').toggleClass('menu-short');
+	// 		$('.scroll-wrap').toggleClass('menu-wide');
+	// 		$('.menu-title').toggleClass('menu-title-active');
+	// 		$('.featured-sub-menu').removeClass('show-sub-menu');
+	// 		$('.featured').find('.arrow-top1').removeClass('flip-off');
+	// 		$('.featured').find('.arrow-bottom1').removeClass('flip-off');
+	// 		$('.featured').find('.arrow-top2').removeClass('flip-on');
+	// 		$('.featured').find('.arrow-bottom2').removeClass('flip-on');
+	// 		$('.drawing-sub-menu').removeClass('show-sub-menu');
+	// 		$('.drawing').find('.arrow-top1').removeClass('flip-off');
+	// 		$('.drawing').find('.arrow-bottom1').removeClass('flip-off');
+	// 		$('.drawing').find('.arrow-top2').removeClass('flip-on');
+	// 		$('.drawing').find('.arrow-bottom2').removeClass('flip-on');
+	// 		$('.design-sub-menu').removeClass('show-sub-menu');
+	// 		$('.design').find('.arrow-top1').removeClass('flip-off');
+	// 		$('.design').find('.arrow-bottom1').removeClass('flip-off');
+	// 		$('.design').find('.arrow-top2').removeClass('flip-on');
+	// 		$('.design').find('.arrow-bottom2').removeClass('flip-on');
+	// 		$('.design span').removeClass('menu-selected');
+	// 		$('.featured span').removeClass('menu-selected');
+	// 		$('.drawing span').removeClass('menu-selected');
+	// 		$('.about-right').toggleClass('menu-wide');
+	// 		$('.about-left').toggleClass('menu-short');
+	// 	});
+
+	// 	$('.featured span').click(function(){
+	// 		$('.featured-sub-menu').toggleClass('show-sub-menu');
+	// 		$('.featured').find('.arrow-top1').toggleClass('flip-off');
+	// 		$('.featured').find('.arrow-bottom1').toggleClass('flip-off');
+	// 		$('.featured').find('.arrow-top2').toggleClass('flip-on');
+	// 		$('.featured').find('.arrow-bottom2').toggleClass('flip-on');
+	// 		$(this).toggleClass('menu-selected');
+
+	// 	});
+
+	// 	$('.drawing span').click(function(){
+	// 		$('.drawing-sub-menu').toggleClass('show-sub-menu');
+	// 		$('.drawing').find('.arrow-top1').toggleClass('flip-off');
+	// 		$('.drawing').find('.arrow-bottom1').toggleClass('flip-off');
+	// 		$('.drawing').find('.arrow-top2').toggleClass('flip-on');
+	// 		$('.drawing').find('.arrow-bottom2').toggleClass('flip-on');
+	// 		$(this).toggleClass('menu-selected');
+	// 	});
+
+	// 	$('.design span').click(function(){
+	// 		$('.design-sub-menu').toggleClass('show-sub-menu');
+	// 		$('.design').find('.arrow-top1').toggleClass('flip-off');
+	// 		$('.design').find('.arrow-bottom1').toggleClass('flip-off');
+	// 		$('.design').find('.arrow-top2').toggleClass('flip-on');
+	// 		$('.design').find('.arrow-bottom2').toggleClass('flip-on');
+	// 		$(this).toggleClass('menu-selected');
+	// 	});
+
+
+	// 	$('.featured svg').click(function(){
+	// 		$('.featured-sub-menu').toggleClass('show-sub-menu');
+	// 		$('.featured').find('.arrow-top1').toggleClass('flip-off');
+	// 		$('.featured').find('.arrow-bottom1').toggleClass('flip-off');
+	// 		$('.featured').find('.arrow-top2').toggleClass('flip-on');
+	// 		$('.featured').find('.arrow-bottom2').toggleClass('flip-on');
+	// 		$('.featured span').toggleClass('menu-selected');
+	// 	});
+
+	// 	$('.drawing svg').click(function(){
+	// 		$('.drawing-sub-menu').toggleClass('show-sub-menu');
+	// 		$('.drawing').find('.arrow-top1').toggleClass('flip-off');
+	// 		$('.drawing').find('.arrow-bottom1').toggleClass('flip-off');
+	// 		$('.drawing').find('.arrow-top2').toggleClass('flip-on');
+	// 		$('.drawing').find('.arrow-bottom2').toggleClass('flip-on');
+	// 		$('.drawing span').toggleClass('menu-selected');
+	// 	});
+
+
+	// 	$('.design svg').click(function(){
+	// 		$('.design-sub-menu').toggleClass('show-sub-menu');
+	// 		$('.design').find('.arrow-top1').toggleClass('flip-off');
+	// 		$('.design').find('.arrow-bottom1').toggleClass('flip-off');
+	// 		$('.design').find('.arrow-top2').toggleClass('flip-on');
+	// 		$('.design').find('.arrow-bottom2').toggleClass('flip-on');
+	// 		$('.design span').toggleClass('menu-selected');
+	// 	});
+	// });
+
 	$('.clickoff').click(function(){
 		hamburger.classList.toggle('is-active');
 		$('.menu').toggleClass('open');
@@ -43,7 +137,7 @@ $(document).ready(function(){
 		$('.about-right').toggleClass('menu-wide');
 		$('.about-left').toggleClass('menu-short');
 	});
-	
+
 	$('.caption-button').click(function(){
 		$(this).toggleClass('caption-expand');
 		$(this).find('div').toggleClass('caption-visible');
